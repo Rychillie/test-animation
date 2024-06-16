f1620e412ce5122eeb781c36e088c542fcebc2b4
Rychillie <14102456+Rychillie@users.noreply.github.com>
2024-06-16 12:32:39 -0300

    Basic Config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,45 @@
-import type { Config } from "tailwindcss";
+import typograhpy from '@tailwindcss/typography'
+import type { Config } from 'tailwindcss'
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+export default {
+	content: ['./src/**/*.{ts,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['var(--font-geist-sans)'],
+				mono: ['var(--font-geist-mono)'],
+			},
+			colors: {
+				primary: '#161616',
+				secondary: '#6F6F6F',
+				'primary-dark': '#EDEDED',
+				'secondary-dark': '#A0A0A0',
+			},
+			spacing: {
+				'page-top': 'var(--page-top)',
+				'page-top-mobile': 'var(--page-top-mobile)',
+				'page-bottom': 'var(--page-bottom)',
+				'page-bottom-mobile': 'var(--page-bottom-mobile)',
+			},
+			keyframes: {
+				in: {
+					'0%': { transform: 'translateY(18px)', opacity: '0' },
+					'100%': { transform: 'translateY(0)', opacity: '1' },
+				},
+				'in-reverse': {
+					'0%': { transform: 'translateY(-18px)', opacity: '0' },
+					'100%': { transform: 'translateY(0px)', opacity: '1' },
+				},
+			},
+			animation: {
+				in: 'in .6s both',
+				'in-reverse': 'in-reverse .6s both',
+			},
+		},
+	},
+	darkMode: 'class',
+	future: {
+		hoverOnlyWhenSupported: true,
+	},
+	plugins: [typograhpy],
+} satisfies Config
